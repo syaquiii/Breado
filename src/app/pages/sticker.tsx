529ce50a3d025d094d04aef09391c52ea820381c
Syauqi oqi <syauqi.imd@gmail.com>
2024-10-05 11:15:47 +0700

    test 1

--- src/app/pages/sticker.tsx
+++ src/app/pages/sticker.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import ImgSticker from "../assets/img-sticker.svg";
 
 const sticker = () => {
+  const buttonList = [{}];
   return (
     <div className="bg-ourgray min-h-[45rem] w-full  ">
       <div className="lg:gap-10 lg:flex px-12 lg:px-32 py-20 lg:py-36 ">
@@ -18,10 +19,13 @@ const sticker = () => {
           <h1 className="lg:text-[4rem] lg:leading-[4rem] text-4xl font-bold">
             Premium bread, crafted with care.
           </h1>
-          <p className="lg:w-[90%] mt-3 text-3xl">
+          <p className="lg:w-[90%] mt-5 text-2xl  lg:text-4xl lg:font-normal font-light">
             Our breads are made with quality and flavor in every bite, made
             fresh from the oven for a more premium taste.
           </p>
+          <button className="bg-ourwhite px-4 py-2 w-full text-ourgray font-semibold  rounded-xl mt-5">
+            100+ store around the world
+          </button>
         </div>
       </div>
     </div>
